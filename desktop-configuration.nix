c293896ab5a5033a2560922818cec49a6d9c8405
Maks <maks.razrushitel08@gmail.com>
2023-07-13 17:02:38 +0300

    first commit

---
+++ desktop-configuration.nix
@@ -0,0 +1,78 @@
+# Configuration for my desktop
+{ config, lib, pkgs, ... }:
+
+{
+  fileSystems."/home/mask/shit" =
+    { device = "/dev/disk/by-label/doc";
+      fsType = "ext4";
+    };
+
+  # Nvidia videocard
+  # Make sure opengl is enabled
+  hardware.opengl = {
+    enable = true;
+  };
+
+  # NVIDIA drivers are unfree.
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "nvidia-x11"
+    ];
+
+  services.xserver.videoDrivers = [ "nvidia" "intel" ];
+
+  hardware.nvidia = {
+
+    # Modesetting is needed for most wayland compositors
+    modesetting.enable = true;
+    powerManagement.enable = true;
+
+    # Use the open source version of the kernel module
+    # Only available on driver 515.43.04+
+    # open = true;
+
+    # Enable the nvidia settings menu
+    # nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+  services.xserver.config = lib.mkForce ''
+  Section "Device"
+    Identifier  "GeForce GTX 750 Ti"
+    Driver      "nvidia"
+
+    # If you have multiple video cards, the BusID controls which one this definition refers
+    # to.  You can omit it if you only have one card.
+    # BusID       "PCI:1:0:0"
+
+    # Need to flag this as only referring to one output on the card
+    Screen      0
+
+    # For nVidia devices, this controls which connector the monitor is connected to.
+    Option      "UseDisplayDevice"   "VGA-2"
+
+    # We want control!
+    Option      "DynamicTwinView"    "FALSE"
+
+    # Various performance and configuration options
+    Option      "AddARGBGLXVisuals"  "true"
+    Option      "UseEDIDDpi"         "false"
+    Option      "DPI"                "96 x 96"
+    Option      "Coolbits"           "1"
+  EndSection
+  Section "Device"
+    Identifier "Intel Graphics"
+    Driver "intel"
+    Option "TearFree" "true"
+  EndSection
+  '';
+
+  # monitors setup
+  services.xserver.displayManager.setupCommands = ''
+    ${pkgs.xorg.xrandr}/bin/xrandr --output DVI-D-1 --off --output VGA-0 --primary --mode 1920x1080 --pos 1680x0 --rotate normal --output HDMI-2 --off --output VGA1 --mode 1680x1050 --pos 0x240 --rotate normal --output HDMI-1-1 --off
+  '';
+  # sound
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+}
---
+++ general-configuration.nix
@@ -0,0 +1,170 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # if its desktop
+  imports = if (builtins.readFile /sys/class/dmi/id/chassis_type) == "3\n" then 
+    [ ./desktop-configuration.nix /etc/nixos/hardware-configuration.nix ] 
+  else 
+    [ ./laptop-configuration.nix /etc/nixos/hardware-configuration.nix] 
+  ;
+
+  networking.hostName = "DM"; # Define your hostname.
+
+  # Configure keymap and i3 in X11
+  services.xserver = {
+    enable = true;
+    layout = "us,ru";
+    xkbVariant = "";
+    xkbOptions = "grp:win_space_toggle";
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+
+    displayManager = {
+      defaultSession = "none+i3";
+    };
+
+    windowManager.i3={
+      enable = true;
+      extraPackages = with pkgs; [
+        albert
+        i3blocks
+        picom-next
+      ];
+      package = pkgs.i3-rounded;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    wget
+    neofetch 
+
+    kitty
+
+    spotify
+
+    firefox
+
+    zoxide
+    colorpanes
+    cfonts
+
+    exa # modern ls
+
+    git
+    gh
+
+    stow # for dotfiles
+
+    arandr # for monitor configuration
+
+    ranger
+
+    pavucontrol
+    
+    wireguard-tools
+    shadowsocks-rust
+
+    deluge # torrent
+
+    killall
+
+    playerctl # for music-control buttons on keyboard
+
+
+    bottles
+    dconf # fixed issue
+
+    unrar
+    unzip
+
+    cmatrix
+
+    xclip
+
+    ffmpeg
+
+    scrot
+
+
+    gnat # gcc
+    clang-tools
+
+    rustc
+    cargo
+
+    godot_4
+
+    btop
+
+    # python packages
+    (python3.withPackages(ps: with ps; [
+      dbus-python
+    ]))
+  ];
+  fonts = {
+    fonts = with pkgs; [
+      powerline-fonts
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+    ];
+  };
+  # shell
+  environment.shells = with pkgs; [ zsh ];
+  programs.zsh = {
+    enable = true;
+    syntaxHighlighting.enable = true;
+    autosuggestions.enable = true;
+
+    shellAliases = {
+      update = "sudo nixos-rebuild switch";
+      nvimconfig="nvim ~/.config/nvim/init.lua";
+      nvimplugins="nvim ~/.config/nvim/lua/plugins.lua";
+      nvimsettings="nvim ~/.config/nvim/lua/settings.lua";
+      ls="exa";
+    };
+    interactiveShellInit = ''
+      # cfonts "ABOBA|PC" --align center --colors green,red --letter-spacing 2 --spaceless
+      colorpanes 
+      eval "$(zoxide init zsh)"
+      autoload -U promptinit; promptinit
+    '';
+    ohMyZsh = {
+      enable = true;
+      customPkgs = with pkgs; [
+        zsh-z # z command instead of cd
+      ];
+      plugins = [ "git" ];
+      # theme = "sporty_256";
+      theme = "agnoster";
+    };
+  };
+  users.defaultUserShell = pkgs.zsh;
+
+  services.picom = {
+    enable = true;
+    backend = "glx";
+		settings = {
+      blur = { 
+        method = "dual_kawase";
+        size = 20;
+        deviation = 5.0;
+      };
+      fading = true;
+      fade-delta = 5;
+      fade-in-step = 0.03;
+      fade-out-step = 0.03;
+		};
+    opacityRules = [
+      "90:class_g = 'kitty' && focused"
+      "85:class_g = 'kitty' && !focused"
+    ];
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+}
